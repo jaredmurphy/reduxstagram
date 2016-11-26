8497a74aaf697b3c739742d955d3ab9d233b41a4
Jared Murphy <jaredcmurphy@gmail.com>
2016-11-26 01:44:14 -0500

    add jsx for photogrid - vid 11

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
 import { render } from 'react-dom';
 
 import css from './styles/style.styl';
 
-import Main from './components/Main';
+import App from './components/App';
 import PhotoGrid from './components/PhotoGrid';
 import Single from './components/Single';
 
+
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="/view/:postId" component={Single}></Route>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
 )
 
 
