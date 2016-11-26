f8854db48ba5b7718e48d23d4fb448631c5765c0
Jared Murphy <jaredcmurphy@gmail.com>
2016-11-25 23:52:41 -0500

    add main, single, and photogrid comps

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,22 @@
-// let's go!
+import React from 'react';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { render } from 'react-dom';
+
+import css from './styles/style.styl';
+
+import Main from './components/Main';
+import PhotoGrid from './components/PhotoGrid';
+import Single from './components/Single';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+
+render(router, document.getElementById('root'));
+
