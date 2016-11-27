190b310a48c18db65fa8843e75072aa3474c7f42
Jared Murphy <jaredcmurphy@gmail.com>
2016-11-26 19:43:20 -0500

    increments likes

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,5 +1,13 @@
 export default function posts(state = [], action) {
-  console.log("the post will change");
-  console.log(state, action);
-  return state;
+  switch(action.type) {
+    case 'INCREMENT_LIKES':
+      const index = action.index;
+      return [
+        ...state.slice(0, index), // before the one we're updating
+        { ...state[index], likes: state[index].likes + 1},
+        ...state.slice(index + 1), // after the one we're updating
+      ];
+    default:
+      return state
+  }
 }
